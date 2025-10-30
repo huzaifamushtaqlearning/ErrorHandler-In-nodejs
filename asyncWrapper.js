0254006c1188bb11ef59dec2d0199b3ec1ca6572
Huzaifa Mushtaq (Learning Profile) <raisaafsha@gmail.com>
2025-10-30 20:55:38 +0500

    Add files via upload

---
+++ asyncWrapper.js
@@ -0,0 +1,12 @@
+const asyncWrapper = (requestHandler = async() => {}) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export { asyncWrapper }
+
+
+
+
