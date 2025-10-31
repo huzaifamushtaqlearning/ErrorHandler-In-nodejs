44a0441d69fffda48ea9819b5af06c7dc8e4a723
Huzaifa Mushtaq (Learning Profile) <raisaafsha@gmail.com>
2025-10-31 23:01:17 +0500

    Update README.md
    Update README.md

--- globalErrorHandler.js
+++ globalErrorHandler.js
@@ -22,3 +22,23 @@ export function globalErrorHandler(err, req, res,next) {
       new ApiRes(error.statusCode, error.data, error._message ?? error.message)
     );
 }
+//------------------------------------------------
+import { ApiError } from "./ApiError.js";
+import { ApiRes } from "./ApiRes.js";
+
+export const globalErrorHandler = (error, req, res, next) => {
+  if (!(error instanceof ApiError)) {
+    res
+      .status(500)
+      .json(
+        new ApiRes(
+          500,
+          null,
+          error._message ?? error.message ?? "Internal Server Error"
+        )
+      );
+  }
+  res
+    .status(error.statusCode)
+    .json(new ApiRes(error.statusCode, error.data, error.message));
+};
